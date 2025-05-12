ab397172a6d9aceabbf83492f57458494411c361
Christen <christen.foad@gmail.com>
2025-05-12 11:58:16 +0300

    Add project files.

---
+++ BlazorApp1/Pages/DepartmentComponent.razor.cs
@@ -0,0 +1,31 @@
+﻿using BlazorApp1.Models;
+
+namespace BlazorApp1.Pages
+{
+    public partial class DepartmentComponent
+    {
+        public List<Department> DeptList { get; set; }
+        //property
+        public Department Dept { get; set; }
+
+        public int DeptID { get; set; } //Selected DEpartment ID
+
+
+        public DepartmentComponent()
+        {
+            //Dept=new Department() { Id=1,Name="SD",ManagerName="Ahemd"};
+            DeptList = new List<Department>() { 
+                new (){ Id=1,Name="SD",ManagerName="Ahemd"},
+                new (){ Id=2,Name="OS",ManagerName="Sara"},
+                new (){ Id=3,Name="GIS",ManagerName="Mohamed"},
+                new (){ Id=4,Name="UI",ManagerName="Hassan"},
+                new (){ Id=5,Name="Digital Marketing",ManagerName="Esraa"},
+            };
+        }
+
+        public void GetDepartment()
+        {
+            Dept = DeptList.FirstOrDefault(d=>d.Id== DeptID);
+        }
+    }
+}
---
+++ BlazorApp1/Program.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace BlazorApp1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents.Add<App>("#app");//index.html ==>wwwroot
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+            //built in Compoennt Change titile per PageComponent
+
+            //day 3
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
